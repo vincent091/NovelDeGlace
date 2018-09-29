d9ede9b6d1d51456a6292c0bb6c3a5366601c524
Vincent Jean-Pierre <vincent@pc23.home>
2018-09-29 22:17:07 +0200

    Add navigation drawer

--- NdG/app/src/main/java/com/lnproduction/noveldeglace/activity/MainActivity.kt
+++ NdG/app/src/main/java/com/lnproduction/noveldeglace/activity/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.lnproduction.noveldeglace.activity
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.lnproduction.noveldeglace.R
 import com.lnproduction.noveldeglace.utils.Constants
 import com.lnproduction.noveldeglace.utils.log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.navigation_main.*
 
-class MainActivity : BaseActivity() {
+
+
+
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
 
     companion object {
@@ -25,14 +36,39 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar as Toolbar?)
+        setContentView(R.layout.navigation_main)
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().run {
                 replace(R.id.sample_content_fragment, PostFragment())
                 commit()
             }
         }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+        val hView = navigationView.getHeaderView(0)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.avatar_demo)
+
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+
+        rounded.setCornerRadius(bitmap.width.toFloat())
+
+        val drawerProfile = hView.findViewById(R.id.imageView) as ImageView
+        drawerProfile.setImageDrawable(rounded)
+
+        val drawerName = hView.findViewById(R.id.full_name) as TextView
+        drawerName.setText("Claramiel")
+
+        val userRole = hView.findViewById(R.id.assignment_role) as TextView
+        userRole.setText("Lolli gothique")
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -51,12 +87,47 @@ class MainActivity : BaseActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     override fun onResume() {
         super.onResume()
     }
 
     override fun onPause() {
         super.onPause()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        /*when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }*/
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
